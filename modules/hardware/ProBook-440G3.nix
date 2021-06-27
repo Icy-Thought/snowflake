aea930bbb19bcc2ca37edb3f940d95a0bfc0a69c
Icy-Thought <gilganyx@pm.me>
2021-06-27 08:53:37 +0200

    cleanup + flake.lock

--- modules/hardware/ProBook-440G3.nix
+++ modules/hardware/ProBook-440G3.nix
@@ -4,25 +4,32 @@
 { config, lib, pkgs, modulesPath, ... }:
 
 {
-  imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
+  imports =
+    [ (modulesPath + "/installer/scan/not-detected.nix")
+    ];
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
   boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-intel" ];
+  boot.kernelModules = [ ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/6a76fd5f-b327-43fb-81cd-aef0c69deb7a";
+    { device = "/dev/disk/by-uuid/42c5c3e5-38df-4007-9fff-5c9841c93a0a";
+      fsType = "ext4";
+    };
+
+  fileSystems."/home" =
+    { device = "/dev/disk/by-uuid/e688ecea-6ebd-4740-bd65-6bc27ae2c0db";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/9C99-45AA";
+    { device = "/dev/disk/by-uuid/097C-54E2";
       fsType = "vfat";
     };
 
   swapDevices =
-    [ { device = "/dev/disk/by-uuid/60245f45-dc70-4d5e-949b-062a93d1f3f2"; }
+    [ { device = "/dev/disk/by-uuid/c65ddf1a-ebcd-4cb4-97c1-02a4bedb8d29"; }
     ];
 
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
