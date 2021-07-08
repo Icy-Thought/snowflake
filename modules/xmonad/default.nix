561443c4019fecb5109d52f2c4a2b15fd164477b
Icy-Thought <gilganyx@pm.me>
2021-07-08 09:36:14 +0200

    [+]: revision

--- modules/xmonad/default.nix
+++ modules/xmonad/default.nix
@@ -32,21 +32,19 @@ in {
             xmonad = pkgs.fetchFromGitHub {
               owner = "xmonad";
               repo = "xmonad";
-              rev = "master";
+              rev = "af354f7528ada1de451365a0f5138ef10a318360";
               sha256 = "sq+hDiMfzfYRvzYucpmNt4u60QT9HXH+rJ89jptyMSI=";
             };
-
             xmonad-contrib = pkgs.fetchFromGitHub {
               owner = "xmonad";
               repo = "xmonad-contrib";
-              rev = "master";
+              rev = "1351f9a931f53e9f1e16c566c70cb8fa98f97785";
               sha256 = "ZX7YU/mp/ORufbL4whnD1vBXVcMqOv8aN+x+lQ7HdOo=";
             };
-
             xmonad-extras = pkgs.fetchFromGitHub {
               owner = "xmonad";
               repo = "xmonad-extras";
-              rev = "master";
+              rev = "6df82de88474754bc90724251d5fcbeccccbd7e7";
               sha256 = "6ikDQVj4Ua8aVTRqugwDYgxltnr7G5KH4cCjdu7vSM4=";
             };
           });
@@ -54,8 +52,8 @@ in {
         extraPackages = haskellPackages: [
           haskellPackages.xmonad
           haskellPackages.xmonad-extras
-          haskellPackages.xmobar
           haskellPackages.xmonad-contrib
+          haskellPackages.xmobar
         ];
       };
     };
