fd3ab72469e15866d51a4127ce13817c0a9a903c
Icy-Thought <gilganyx@pm.me>
2021-07-09 13:30:30 +0200

    [^]: successful xmonad build, fix conf

--- modules/xmonad/passage.nix
+++ modules/xmonad/passage.nix
@@ -10,12 +10,10 @@
 
   xsession = {
     enable = true;
-    # initExtra = extra + taffybar;
 
     windowManager.xmonad = {
       enable = true;
       enableContribAndExtras = true;
-      config = ./xmonad.hs;
 
       extraPackages = haskellPackages: [
         haskellPackages.dbus
@@ -23,22 +21,4 @@
       ];
     };
   };
-
-  home.file = {
-    ".xmonad/xmobar.hs" = {
-      source = ./xmobar.hs;
-      onChange = ''
-        if [[ -v DISPLAY ]] ; then
-          echo "Recompiling xmobar"
-          xmobar -r ~/.xmonad/xmobar.hs &
-          sleep 2
-          disown
-          killall xmobar
-          echo "Restarting"
-          xmonad --restart
-        fi
-      '';
-    };
-  };
-
 }
--- overlays/xmonad.nix
+++ overlays/xmonad.nix
@@ -1,40 +1,40 @@
-self: super: {
+self: super:
+
+let
+  xmonadGH = self.fetchFromGitHub {
+    owner = "xmonad";
+    repo = "xmonad";
+    rev = "af354f7528ada1de451365a0f5138ef10a318360";
+    sha256 = "sq+hDiMfzfYRvzYucpmNt4u60QT9HXH+rJ89jptyMSI=";
+    fetchSubmodules = true;
+  };
+
+  xmonad-extrasGH = self.fetchFromGitHub {
+    owner = "xmonad";
+    repo = "xmonad-extras";
+    rev = "6df82de88474754bc90724251d5fcbeccccbd7e7";
+    sha256 = "1kj8xzp7d8y0w63r46zvgav6a3320c6blsilaldaylgqb10h6aga";
+    fetchSubmodules = false;
+  };
+
+  xmonad-contribGH = self.fetchFromGitHub {
+    owner = "xmonad";
+    repo = "xmonad-contrib";
+    rev = "1351f9a931f53e9f1e16c566c70cb8fa98f97785";
+    sha256 = "ZX7YU/mp/ORufbL4whnD1vBXVcMqOv8aN+x+lQ7HdOo=";
+    fetchSubmodules = true;
+  };
+
+in {
   haskellPackages = super.haskellPackages.override (old: {
-    overrides = with self.haskell.lib;
-      self.lib.composeExtensions (old.overrides or (_: _: { }))
-      (hself: hsuper: {
-        xmonad = dontCheck (overrideSrc hsuper.xmonad {
-          version = "0.16.9999";
-          src = self.fetchFromGitHub {
-            owner = "xmonad";
-            repo = "xmonad";
-            rev = "af354f7528ada1de451365a0f5138ef10a318360";
-            sha256 = "sq+hDiMfzfYRvzYucpmNt4u60QT9HXH+rJ89jptyMSI=";
-            fetchSubmodules = true;
-          };
-        });
-
-        xmonad-extras = dontCheck (overrideSrc hsuper.xmonad-extras {
-          version = "0.15.3";
-          src = self.fetchFromGitHub {
-            owner = "xmonad";
-            repo = "xmonad-extras";
-            rev = "6df82de88474754bc90724251d5fcbeccccbd7e7";
-            sha256 = "1kj8xzp7d8y0w63r46zvgav6a3320c6blsilaldaylgqb10h6aga";
-            fetchSubmodules = false;
-          };
-        });
-
-        xmonad-contrib = dontCheck (overrideSrc hsuper.xmonad-contrib {
-          version = "0.17";
-          src = self.fetchFromGitHub {
-            owner = "xmonad";
-            repo = "xmonad-contrib";
-            rev = "1351f9a931f53e9f1e16c566c70cb8fa98f97785";
-            sha256 = "ZX7YU/mp/ORufbL4whnD1vBXVcMqOv8aN+x+lQ7HdOo=";
-            fetchSubmodules = true;
-          };
-        });
+    overrides = super.lib.composeExtensions (old.overrides or (_: _: { }))
+      (hself: hsuper: rec {
+        xmonad = hself.callCabal2nix "xmonad" xmonadGH { };
+        xmonad-extras = hself.callCabal2nix "xmonad-extras" xmonad-extrasGH { };
+        xmonad-contrib =
+          hself.callCabal2nix "xmonad-contrib" xmonad-contribGH { };
+        xmonad-config =
+          hself.callPackage ../modules/xmonad/xmonad-config.nix { };
       });
   });
 }
