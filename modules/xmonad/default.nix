a2750abecd300fe3888bf740023bcad62dad96ad
Icy-Thought <gilganyx@pm.me>
2021-07-09 13:30:30 +0200

    [^]: successful xmonad build, fix conf

--- modules/xmonad/default.nix
+++ modules/xmonad/default.nix
@@ -1,7 +1,6 @@
 { config, lib, pkgs, ... }:
 
 let
-
   xmonadPkgs = with pkgs; [
     (rofi.override { plugins = [ rofi-emoji rofi-calc ]; }) # dmenu alt.
     dunst # Notification tool.
@@ -26,17 +25,6 @@ in {
       windowManager.xmonad = {
         enable = true;
         enableContribAndExtras = true;
-        haskellPackages = with pkgs; [
-          haskellPackages.xmobar
-          (haskellPackages.override {
-            overrides = haskellPackagesNew: haskellPackagesOld: rec {
-              xmonad = pkgs.haskell.lib.dontCheck haskellPackagesNew.xmonad;
-              xmonad-extras = haskellPackagesNew.lib.dontCheck (xmonad-extras);
-              xmonad-contrib =
-                haskellPackagesNew.lib.dontCheck (xmonad-contrib);
-            };
-          })
-        ];
       };
     };
   };
