979d40be77197474f01de872e1038d5a00b6dfa8
Ivan Malison <IvanMalison@gmail.com>
2021-07-27 20:23:43 -0600

    Simpler xmonad setup

--- modules/window-managers/xmonad/config/xmonad/overlay.nix
+++ modules/window-managers/xmonad/config/xmonad/overlay.nix
@@ -1,20 +1,22 @@
 _: pkgs: rec {
   haskellPackages = pkgs.haskellPackages.override (old: {
-    overrides = pkgs.lib.composeExtensions (old.overrides or (_: _: { }))
-      (final: prev: rec {
-        xmonad = prev.callCabal2nix "xmonad" (pkgs.fetchFromGitHub {
+    overrides = pkgs.lib.composeExtensions (old.overrides or (_: _: {})) (final: prev: rec {
+      icy-xmonad = final.callCabal2nix "icy-xmonad" (
+        ./.
+      ) { };
+      xmonad = final.callCabal2nix "xmonad" (pkgs.fetchFromGitHub {
           owner = "xmonad";
           repo = "xmonad";
           rev = "05aeef0dc2ef84d92f2d3dec6cd3acdecb4c9851";
           sha256 = "vRxjhRJqu5N4/+Xd4prxssjC4bYvLmpJpSbtUYCCYBo=";
         }) { };
-        xmonad-contrib = prev.callCabal2nix "xmonad-contrib"
+        xmonad-contrib = final.callCabal2nix "xmonad-contrib"
           (pkgs.fetchFromGitHub {
             owner = "xmonad";
             repo = "xmonad-contrib";
             rev = "4c759ff70cd2105cfe39bd682056f4890001a844";
             sha256 = "SwLgUOEa5TPaS5vYx2CAwC15yNlziMYAkNfSUNOpAjg=";
           }) { };
-      });
+    });
   });
 }
