b9f16a3e8026f184b3b84a2463c5c176f4c74dbf
Icy-Thought <gilganyx@pm.me>
2021-07-28 02:14:31 +0200

    [^]: testing league solution..

--- packages/home-manager/gaming.nix
+++ packages/home-manager/gaming.nix
@@ -2,9 +2,13 @@
 
 let
   gamingPkgs = with pkgs; [
-    wine-staging # Latest Wine Package.
     lutris # WINE Gaming Platform.
     osu-lazer # FOSS Rythm Game!
   ];
 
-in { home.packages = gamingPkgs; }
+  winePkgs = with pkgs.winePackages; [
+    staging # Wine-Staging.
+    fonts # MS-fonts by wine-project.
+  ];
+
+in { home.packages = gamingPkgs ++ winePkgs; }
