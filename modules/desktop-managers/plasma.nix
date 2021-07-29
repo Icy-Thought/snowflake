d64f590547268696f132d28493541acee19fb006
Icy-Thought <gilganyx@pm.me>
2021-07-29 08:07:08 +0200

    [+]: moved display-managers to indv. folder

--- modules/desktop-managers/plasma.nix
+++ modules/desktop-managers/plasma.nix
@@ -1,8 +1,7 @@
 { config, lib, pkgs, ... }:
 
 {
-  services.xserver = {
-    displayManager.sddm.enable = true;
-    desktopManager.plasma5.enable = true;
-  };
+  imports = [ ../display-managers/sddm.nix ];
+
+  services.xserver.desktopManager.plasma5.enable = true;
 }
