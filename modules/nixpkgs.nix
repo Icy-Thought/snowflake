2a098c1b84c1e21e2b8f02356303d4d262159c30
Icy-Thought <gilganyx@pm.me>
2021-08-09 15:01:26 +0200

    [^]: correct cachnix url.. caching finally works.

--- modules/nixpkgs.nix
+++ modules/nixpkgs.nix
@@ -31,10 +31,9 @@
       "home-manager=/etc/${config.environment.etc.home-manager.target}"
     ];
 
-    binaryCaches = [ "https://nix-community.cachix.org/" ];
-    binaryCachePublicKeys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    ];
+    binaryCaches = [ "https://cache.nixos.org/" ];
+    binaryCachePublicKeys =
+      [ "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=" ];
 
     registry = { nixpkgs.flake = inputs.nixpkgs; };
 
