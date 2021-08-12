227940134963b4c8162d393b93ff6a74a3c6f8f2
Icy-Thought <gilganyx@pm.me>
2021-08-12 23:42:05 +0200

    [+]: proper onedark colors.

--- config/zathura/default.nix
+++ config/zathura/default.nix
@@ -10,36 +10,43 @@
       window-title-basename = true;
 
       # Base16 OneDark
-      default-bg = "#282C34";
-      default-fg = "#353B45";
+      default-bg = "#1E2127";
+      default-fg = "#ABB2BF";
 
-      statusbar-fg = "#565C64";
-      statusbar-bg = "#3E4451";
-      inputbar-bg = "#282C34";
-      inputbar-fg = "#C8CCD4";
+      statusbar-bg = "#2C323C";
+      statusbar-fg = "#ABB2BF";
+      inputbar-bg = "#1E2127";
+      inputbar-fg = "#ABB2BF";
 
-      notification-bg = "#282C34";
-      notification-fg = "#C8CCD4";
-      notification-error-bg = "#282C34";
-      notification-error-fg = "#E06C75";
-      notification-warning-bg = "#282C34";
-      notification-warning-fg = "#E06C75";
+      notification-bg = "#98C379";
+      notification-fg = "#1E2127";
+      notification-error-bg = "#E06C75";
+      notification-error-fg = "#1E2127";
+      notification-warning-bg = "#E5C07B";
+      notification-warning-fg = "#1E2127";
 
-      # Highlight:
-      highlight-color = "#E5C07B";
-      highlight-active-color = "#61AFEF";
+      highlight-color = "#61AFEF";
+      highlight-active-color = "#C678DD";
 
-      # Completion:
-      completion-highlight-fg = "#353B45";
+      completion-bg = "#2C323C";
+      completion-fg = "#ABB2BF";
+      completion-group-bg = "#2C323C";
+      completion-group-fg = "#ABB2BF";
+      completion-highlight-fg = "#2C323C";
       completion-highlight-bg = "#61AFEF";
-      completion-bg = "#C8CCD4";
-      completion-fg = "#61AFEF";
 
-      # Force recolor:
-      recolor-lightcolor = "#282C34";
-      recolor-darkcolor = "#B6BDCA";
+      index-bg = "#1E2127";
+      index-fg = "#ABB2BF";
+      index-active-bg = "#61AFEF";
+      index-active-fg = "#2C323C";
+
+      render-loading-bg = "#2C323C";
+      render-loading-fg = "#61AFEF";
+
+      recolor-lightcolor = "#1E2127";
+      recolor-darkcolor = "#ABB2BF";
       recolor = "true";
-      recolor-keephue = "false";
+      recolor-keephue = "true";
     };
 
   };
