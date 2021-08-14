e52c5e1fdf43f248dacbbe28a8ab54a77f89759d
Icy-Thought <gilganyx@pm.me>
2021-08-14 20:56:01 +0200

    [+]: fixed rofi blur.

--- config/rofi/default.nix
+++ config/rofi/default.nix
@@ -2,7 +2,8 @@
 
   programs.rofi = {
     enable = true;
-    plugins = with pkgs; [ rofi-systemd rofi-emoji rofi-calc ];
+    plugins = with pkgs; [ rofi-systemd ];
+    theme = ./launcher/blurry.rasi;
 
     extraConfig = {
       font = "Noto Sans 10";
@@ -17,108 +18,6 @@
       sidebar-mode = false;
     };
 
-    theme = let inherit (config.lib.formats.rasi) mkLiteral;
-    in {
-      "*" = {
-        background = mkLiteral "#00000000";
-        background-alt = mkLiteral "#00000000";
-        background-bar = mkLiteral "#f2f2f215";
-        foreground = mkLiteral "#f2f2f2EE";
-        accent = mkLiteral "#3DAEE966";
-      };
-
-      "window" = {
-        transparency = "real";
-        background-color = mkLiteral "@background";
-        text-color = mkLiteral "@foreground";
-        border = mkLiteral "0px";
-        border-color = mkLiteral "@border";
-        border-radius = mkLiteral "0px";
-        width = mkLiteral "40%";
-        location = mkLiteral "center";
-        x-offset = mkLiteral "0";
-        y-offset = mkLiteral "0";
-      };
-
-      "prompt" = {
-        enabled = mkLiteral "true";
-        padding = mkLiteral "0.30% 1% 0% -0.5%";
-        background-color = mkLiteral "@background-alt";
-        text-color = mkLiteral "@foreground";
-        font = "FantasqueSansMono Nerd Font 12";
-      };
-
-      "entry" = {
-        background-color = mkLiteral "@background-alt";
-        text-color = mkLiteral "@foreground";
-        placeholder-color = mkLiteral "@foreground";
-        expand = mkLiteral "true";
-        horizontal-align = mkLiteral "0";
-        placeholder = "Search";
-        padding = mkLiteral "0.10% 0% 0% 0%";
-        blink = mkLiteral "true";
-      };
-
-      "inputbar" = {
-        children = map mkLiteral [ "prompt" "entry" ];
-        background-color = mkLiteral "@background-bar";
-        text-color = mkLiteral "@foreground";
-        expand = mkLiteral "false";
-        border = mkLiteral "0% 0% 0% 0%";
-        border-radius = mkLiteral "12px";
-        border-color = mkLiteral "@accent";
-        margin = mkLiteral "0% 0% 0% 0%";
-        padding = mkLiteral "1.5%";
-      };
-
-      "listview" = {
-        background-color = mkLiteral "@background-alt";
-        columns = mkLiteral "5";
-        lines = mkLiteral "3";
-        spacing = mkLiteral "0%";
-        cycle = mkLiteral "false";
-        dynamic = mkLiteral "true";
-        layout = mkLiteral "vertical";
-      };
-
-      "mainbox" = {
-        background-color = mkLiteral "@background-alt";
-        border = mkLiteral "0% 0% 0% 0%";
-        border-radius = mkLiteral "0% 0% 0% 0%";
-        border-color = mkLiteral "@accent";
-        children = map mkLiteral [ "inputbar" "listview" ];
-        spacing = mkLiteral "2%";
-        padding = mkLiteral "2% 1% 2% 1%";
-      };
-
-      "element" = {
-        background-color = mkLiteral "@background-alt";
-        text-color = mkLiteral "@foreground";
-        orientation = mkLiteral "vertical";
-        border-radius = mkLiteral "0%";
-        padding = mkLiteral "2% 0% 2% 0%";
-      };
-
-      "element-icon" = {
-        size = mkLiteral "64px";
-        border = mkLiteral "0px";
-      };
-
-      "element-text" = {
-        expand = mkLiteral "true";
-        horizontal-align = mkLiteral "0.5";
-        vertical-align = mkLiteral "0.5";
-        margin = mkLiteral "0.5% 0.5% -0.5% 0.5%";
-      };
-
-      "element selected" = {
-        background-color = mkLiteral "@background-bar";
-        text-color = mkLiteral "@foreground";
-        border = mkLiteral "0% 0% 0% 0%";
-        border-radius = mkLiteral "12px";
-        border-color = mkLiteral "@accent";
-      };
-    };
   };
 
 }
