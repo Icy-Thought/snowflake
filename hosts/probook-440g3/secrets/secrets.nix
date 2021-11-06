0baac9a28ddeee014f8aedf11b459f3f06df0b22
Icy-Thought <icy-thought@pm.me>
2021-11-07 00:20:04 +0100

    [+]: agenix (ProBook).

--- hosts/probook-440g3/secrets/secrets.nix
+++ hosts/probook-440g3/secrets/secrets.nix
@@ -1,8 +1,5 @@
 let
   orca =
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEhCXXL2cQz2nPQAb2q9olKirOyqDCEcElzl1Nr3QLlQ";
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILd7YveCzR/hQiqpUHMtIvB6wrLbxQasVvekeldrMLt5";
 
-in {
-  # TODO: add after generating the damn file..
-  # "akkadianVPN.age".publicKeys = [ orca ];
-}
+in { "akkadianVPN.age".publicKeys = [ orca ]; }
