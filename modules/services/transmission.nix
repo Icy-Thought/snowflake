3b5d89b7076aa96558b4535468705803c7f452d6
Icy-Thought <icy-thought@pm.me>
2021-12-21 22:48:50 +0100

    [+]: (Temporary) Transmission client -> Remote.

--- modules/services/transmission.nix
+++ modules/services/transmission.nix
@@ -7,32 +7,36 @@ in {
   options.modules.services.transmission = { enable = mkBoolOpt false; };
 
   config = mkIf cfg.enable {
-    user.packages = with pkgs; [ transmission-gtk ];
+    user.packages = with pkgs; [ transmission-remote-gtk ];
 
     services.transmission = {
-      home = "${config.user.home}/Torrents";
+      enable = true;
       settings = {
         # Manage transmisson file locations:
+        download-dir = "/var/lib/transmission/Downloads";
+        incomplete-dir = "/var/lib/transmission/.incomplete";
         rename-partial-files = true;
         incomplete-dir-enabled = true;
         watch-dir-enabled = false;
 
         # Control transmisson ports:
-        peer-port = 51413;
-        peer-port-random-high = 65535;
-        peer-port-random-low = 49152;
-        peer-port-random-on-start = false;
         rpc-port = 9091;
         rpc-whitelist-enabled = true;
         rpc-whitelist = "127.0.0.1,192.168.*.*";
         rpc-bind-address = "0.0.0.0";
 
+        peer-port = 51413;
+        peer-socket-tos = "lowcost";
+
         # Extras settings:
-        message-level = 1;
         umask = 2;
-        utp-enabled = true;
-        ratio-limit = 0;
         encryption = 2;
+        message-level = 1;
+        utp-enabled = true;
+
+        ratio-limit = 0;
+        ratio-limit-enabled = true;
+
         speed-limit-up-enabled = true;
         speed-limit-up = 1;
         idle-seeding-limit-enabled = true;
