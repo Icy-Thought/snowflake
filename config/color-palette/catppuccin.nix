d54e8b8cdc193fea91568d23bfb750062ff832f1
Icy-Thought <icy-thought@pm.me>
2022-01-20 11:59:46 +0100

    [+]: catppuccin color-palette!

---
+++ config/color-palette/catppuccin.nix
@@ -0,0 +1,39 @@
+# TODO: ayu-dark module.
+{
+  # Base Colors
+  foreground = "#D9E0EE";
+  background = "#1E1E2E";
+
+  cursorForeground = "#F5E0DC";
+  cursorBackground = "#1E1E2E";
+
+  selectionForeground = "#D9E0EE";
+  selectionBackground = "#575268";
+
+  black = "#6E6C7E";
+  brightBlack = "#988BA2";
+
+  red = "#F28FAD";
+  brightRed = "#F28FAD";
+
+  green = "#ABE9B3";
+  brightGreen = "#ABE9B3";
+
+  yellow = "#FAE3B0";
+  brightYellow = "#FAE3B0";
+
+  blue = "#96CDFB";
+  brightBlue = "#96CDFB";
+
+  magenta = "#F5C2E7";
+  brightMagenta = "#F5C2E7";
+
+  cyan = "#89DCEB";
+  brightCyan = "#89DCEB";
+
+  white = "#D9E0EE";
+  brightWhite = "#D9E0EE";
+
+  # Zathura-related Colors
+  zathuraBlack = "#1E1D2F";
+}
