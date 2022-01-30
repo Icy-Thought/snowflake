ca7115e0b9eef5b4b0841c58e8d5f5b4ca14bcb5
Icy-Thought <icy-thought@pm.me>
2022-01-30 14:09:04 +0100

    [⚙] (sddm): virtual-keyboard bug..

--- modules/desktop/envDisplay/sddm.nix
+++ modules/desktop/envDisplay/sddm.nix
@@ -11,20 +11,25 @@ in {
 
   config = mkIf cfg.enable (mkMerge [
     {
-      services.xserver = {
+      services.xserver.enable = true;
+
+      services.xserver.displayManager.sddm = {
         enable = true;
-        displayManager.sddm = { enable = true; };
+        extraConfig = ''
+          [General]
+          InputMethod=
+        '';
       };
     }
 
     (mkIf (cfg.themeAerial.enable) {
       services.xserver.displayManager.sddm = {
-        theme = "${(pkgs.fetchFromGitHub {
+        theme = pkgs.fetchFromGitHub {
           owner = "3ximus";
           repo = "aerial-sddm-theme";
           rev = "2fa0a4024bab60b0ba40de274880e0c1aa6eca59";
           sha256 = "jaGQaClD7Hk4eWh+rMX8ZtcGDzb9aCu+NX5gzJ1JXQg=";
-        })}";
+        };
       };
 
       environment.systemPackages = with pkgs; [
