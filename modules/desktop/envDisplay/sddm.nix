658eb96b6cd55044bbdcd6a86a4edb5cd949ef22
Icy-Thought <icy-thought@pm.me>
2022-01-30 17:09:20 +0100

    [✒] (sddm): accidentally removed brackets

--- modules/desktop/envDisplay/sddm.nix
+++ modules/desktop/envDisplay/sddm.nix
@@ -21,12 +21,12 @@ in {
 
     (mkIf (cfg.themeAerial.enable) {
       services.xserver.displayManager.sddm = {
-        theme = pkgs.fetchFromGitHub {
+        theme = "${(pkgs.fetchFromGitHub {
           owner = "3ximus";
           repo = "aerial-sddm-theme";
           rev = "2fa0a4024bab60b0ba40de274880e0c1aa6eca59";
           sha256 = "jaGQaClD7Hk4eWh+rMX8ZtcGDzb9aCu+NX5gzJ1JXQg=";
-        };
+        })}";
       };
 
       environment.systemPackages = with pkgs; [
