9e024ba5b00dcadf5f0fb796b1799201dcd12d1e
Icy-Thought <icy-thought@pm.me>
2022-03-12 23:52:10 +0100

    [🖋] (general): minor system modifications

--- modules/desktop/appliances/editors/emacs.nix
+++ modules/desktop/appliances/editors/emacs.nix
@@ -79,11 +79,10 @@ in {
     # init.doomEmacs = mkIf cfg.doom.enable ''
     #   if [ -d $HOME/.config/emacs ]; then
     #      ${optionalString cfg.doom.fromSSH ''
-    #         git clone git@github.com:hlissner/doom-emacs.git $HOME/.config/emacs
+    #         git clone --depth 1 git@github.com:hlissner/doom-emacs.git $HOME/.config/emacs
     #      ''}
-    #
     #      ${optionalString (cfg.doom.fromSSH == false) ''
-    #         git clone https://github.com/hlissner/doom-emacs $HOME/.config/emacs
+    #         git clone --depth 1 https://github.com/hlissner/doom-emacs $HOME/.config/emacs
     #      ''}
     #   fi
     # '';
