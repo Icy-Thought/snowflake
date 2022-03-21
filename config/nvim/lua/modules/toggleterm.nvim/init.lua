73e4c5cc77aec64de98b9118a885ffaef11fdb15
Icy-Thought <icy-thought@pm.me>
2022-03-21 19:18:53 +0100

    [🚀] (nvim): added toggleterm bindings

--- config/nvim/lua/modules/toggleterm.nvim/init.lua
+++ config/nvim/lua/modules/toggleterm.nvim/init.lua
@@ -1,3 +1,16 @@
+function _G.set_terminal_keymaps()
+  local opts = {noremap = true}
+  vim.api.nvim_buf_set_keymap(0, 't', '<esc>', [[<C-\><C-n>]], opts)
+  vim.api.nvim_buf_set_keymap(0, 't', 'jk', [[<C-\><C-n>]], opts)
+  vim.api.nvim_buf_set_keymap(0, 't', '<C-h>', [[<C-\><C-n><C-W>h]], opts)
+  vim.api.nvim_buf_set_keymap(0, 't', '<C-j>', [[<C-\><C-n><C-W>j]], opts)
+  vim.api.nvim_buf_set_keymap(0, 't', '<C-k>', [[<C-\><C-n><C-W>k]], opts)
+  vim.api.nvim_buf_set_keymap(0, 't', '<C-l>', [[<C-\><C-n><C-W>l]], opts)
+end
+
+-- if you only want these mappings for toggle term use term://*toggleterm#* instead
+vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
+
 local toggleterm = prequire("toggleterm")
 
 toggleterm.setup({
@@ -17,7 +30,7 @@ toggleterm.setup({
     insert_mappings = true,
     terminal_mappings = true,
     persist_size = true,
-    direction = "horizontal",
+    direction = "tab",
     close_on_exit = true,
     shell = vim.o.shell,
     float_opts = {
