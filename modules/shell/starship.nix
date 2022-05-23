abb96465800e57174956933c0e10f0db2ebc54fb
Icy-Thought <icy-thought@pm.me>
2022-05-24 00:10:11 +0200

    [🦚] (starship): status = rounded!

--- modules/shell/starship.nix
+++ modules/shell/starship.nix
@@ -1,28 +1,38 @@
-{
-  config,
-  options,
-  lib,
-  pkgs,
-  ...
+{ config
+, options
+, lib
+, pkgs
+, ...
 }:
 with lib;
 with lib.my; let
   cfg = config.modules.shell.fish;
-in {
+in
+{
   config = mkIf cfg.enable {
     homeManager.programs.starship = {
       enable = true;
       enableBashIntegration = true;
       enableFishIntegration = true;
 
-      settings = {
+      settings = with config.modules.themes; {
         add_newline = true;
         scan_timeout = 10;
 
+        format = "[$directory](fg:${colors.cyan}) [$git_branch](fg:${colors.yellow}) [$git_status](fg:${colors.red}) $character ";
+        directory.format = "[⯁ $path](bg:${colors.cyan} fg:${colors.types.bg} bold)";
+
+        git_branch = {
+          format = "[on $symbol$branch](bg:${colors.yellow} fg:${colors.types.bg} bold)";
+          symbol = " ";
+        };
+
+        git_status.format = "[$all_status](bg:${colors.red} fg:${colors.types.bg} bold)";
+
         character = {
-          success_symbol = "[>>=](bold green)";
-          error_symbol = "[>>=](bold red)";
-          vicmd_symbol = "[=<<](bold blue)";
+          success_symbol = "[](bold green)";
+          error_symbol = "[](bold red)";
+          vicmd_symbol = "[](bold blue)";
         };
 
         battery = {
