36014c0a8b3deeafff7352b58c04c98da3258d45
Icy-Thought <icy-thought@pm.me>
2022-06-08 18:54:01 +0200

    [🚀] (starship): better defaults + fixed empty circ.

--- modules/shell/starship.nix
+++ modules/shell/starship.nix
@@ -23,7 +23,7 @@ in {
         add_newline = true;
         scan_timeout = 10;
 
-        format = "[$directory](fg:${colors.cyan}) $git_branch$git_status$character";
+        format = "[$directory](fg:#89dceb) ($git_branch)($git_status )($nix_shell)($character)";
         directory = {
           style = "bg:${colors.cyan} fg:${colors.types.bg} bold";
           truncation_length = 3;
@@ -32,21 +32,30 @@ in {
         };
 
         git_branch = {
-          style = "bg:${colors.green} fg:${colors.types.bg} bold";
-          format = "[[](bg: ${colors.green})(on $symbol$branch)[](bg: ${colors.green})]($style) ";
+          style = "bg:${colors.yellow} fg:${colors.types.bg} bold";
+          format = "[[](bg: ${colors.yellow})(on $symbol$branch)[](bg: ${colors.yellow})]($style) ";
           symbol = " ";
         };
 
         # TODO: find appealing symbols
         git_status = {
-          style = "bg:${colors.red} fg:${colors.types.bg} bold";
-          format = "[[](bg: ${colors.red})(「$all_status$ahead_behind」)[](bg: ${colors.red})]($style) ";
+          style = "bg:${colors.magenta} fg:${colors.types.bg} bold";
+          format = "[([](bg: ${colors.magenta})「$all_status$ahead_behind」[](bg: ${colors.magenta}))]($style)";
         };
 
         character = {
           success_symbol = "[](bold green)";
           error_symbol = "[](bold red)";
           vicmd_symbol = "[](bold blue)";
+        };
+
+        nix_shell = {
+          disabled = false;
+          impure_msg = "[impure](bold red)";
+          pure_msg = "[pure](bold green)";
+          style = "bold blue";
+          symbol = "[λ ](bold yellow)";
+          format = "via [$symbol$state( \\($name\\))]($style) ";
         };
 
         battery = {
