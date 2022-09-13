22ee72c00be1237d960a8b09cf44d338a0593bea
Icy-Thought <icy-thought@pm.me>
2022-09-13 22:43:23 +0200

    [𝛌] (xmonad): fix formatting

--- config/xmonad/lib/Gibil/Scratchpads.hs
+++ config/xmonad/lib/Gibil/Scratchpads.hs
@@ -3,7 +3,7 @@
 module Gibil.Scratchpads where
 
 import           XMonad
-import qualified XMonad.StackSet             as W
+import qualified XMonad.StackSet               as W
 import           XMonad.Util.NamedScratchpad
 
 -- FIXME
@@ -34,35 +34,39 @@ getLayout = \case
   Medium -> customFloating $ W.RationalRect (1 / 6) (1 / 6) (2 / 3) (2 / 3)
   Large  -> customFloating $ W.RationalRect (1 / 10) (1 / 10) (4 / 5) (4 / 5)
 
-runExternal ∷ MonadIO m ⇒ m a → m a
+runExternal :: MonadIO m => m a -> m a
 runExternal m = do
   uninstallSignalHandlers
   x <- m
   installSignalHandlers
   return x
 
-inTerm cls cwd cmd = C.terminal ++ " -c '" ++ cls ++ "' -d " ++ cwd ++ " -e " ++ cmd
+inTerm cls cwd cmd =
+  C.terminal ++ " -c '" ++ cls ++ "' -d " ++ cwd ++ " -e " ++ cmd
 
 inEditor file = C.editor ++ " " ++ file
 
 newNS (s, cmd, layout) = NS cls cmd (className =? cls) $ getLayout layout
-  where
-    cls = show s
+  where cls = show s
 
-newTerminalNS (s, cmd, layout, cwd) = newNS (s, inTerm (show s) cwd cmd, layout)
+newTerminalNS (s, cmd, layout, cwd) =
+  newNS (s, inTerm (show s) cwd cmd, layout)
 
 -- | List of scratchpads
-scratchpads =
-  map
-    newTerminalNS
-    [ (SystemMonitor, "gotop", Large, "~"),
-      (Notes, inEditor "~/nixos/extras/notes/index.norg", Large, "~/nixos/extras/notes"),
-      (Terminal, "zsh", Medium, "~"),
-      (Calendar, "wyrd", Medium, "~"),
-      (Repl Js, "node", Small, "~"),
-      (Repl RateSx, "~/scripts/rate-sx.sh", Medium, "~"),
-      (Repl Haskell, "ghci", Small, "~")
-    ]
+scratchpads = map
+  newTerminalNS
+  [ (SystemMonitor, "gotop", Large, "~")
+  , ( Notes
+    , inEditor "~/nixos/extras/notes/index.norg"
+    , Large
+    , "~/nixos/extras/notes"
+    )
+  , (Terminal    , "zsh"                 , Medium, "~")
+  , (Calendar    , "wyrd"                , Medium, "~")
+  , (Repl Js     , "node"                , Small , "~")
+  , (Repl RateSx , "~/scripts/rate-sx.sh", Medium, "~")
+  , (Repl Haskell, "ghci"                , Small , "~")
+  ]
 
-scratchpad ∷ Scratchpad → X ()
+scratchpad :: Scratchpad -> X ()
 scratchpad = namedScratchpadAction scratchpads . show
