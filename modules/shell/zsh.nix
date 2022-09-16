1843101fa97878a37f63ade664e4ffa449029413
Icy-Thought <icy-thought@pm.me>
2022-09-16 10:31:32 +0200

    [𝛏] (zsh): don't know anymore.. need help

--- modules/shell/zsh.nix
+++ modules/shell/zsh.nix
@@ -23,13 +23,11 @@ in {
       yt-dlp
       csview
 
-      # alternatives for several gnu-tools
+      # GNU Alternatives
       bottom
       exa
       fd
-      (ripgrep.override {
-        withPCRE2 = true;
-      })
+      (ripgrep.override {withPCRE2 = true;})
       zoxide
     ];
 
@@ -49,10 +47,30 @@ in {
       dotDir = ".config/zsh";
       history = {
         size = 10000;
-        expireDuplicatesFirst = true;
         path = "$XDG_CONFIG_HOME/zsh/history";
       };
       historySubstringSearch.enable = true;
+
+      autocd = true;
+      enableSyntaxHighlighting = true;
+      enableAutosuggestions = true;
+
+      enableCompletion = true;
+      initExtraBeforeCompInit = ''
+        fpath+=(/run/current-system/sw/share/zsh/site-functions)
+        zstyle ':completion:*' completer _complete
+        zstyle ':completion:*' matcher-list "" 'm:{[:lower:][:upper:]-_}={[:upper:][:lower:]_-}' '+l:|=* r:|=*'
+        zstyle ':completion:*' menu select
+        _comp_options+=(globdots)
+        zmodload zsh/complist
+      '';
+
+      localVariables = {
+        KEYTIMEOUT = "1";
+        ZSH_AUTOSUGGEST_STRATEGY = ["completion"];
+        ZSH_AUTOSUGGEST_USE_ASYNC = true;
+        ZSH_AUTOSUGGEST_BUFFER_MAX_SIZE = 40;
+      };
 
       initExtra = ''
         # -------===[ General ]===------- #
@@ -63,9 +81,19 @@ in {
         unsetopt CORRECT_ALL                # Don't correct mis-spellings in args.
         unsetopt NOMATCH                    # Don't print err on no matches.
         unsetopt BEEP                       # Don't disturb the silence.
-        setopt HIST_BEEP                    # Beep on non-existent history access.
         setopt IGNORE_EOF                   # Don't exit on End-Of-File.
         WORDCHARS='_-*?[]~&.;!#$%^(){}<>'   # Special chars == part of a word!
+
+        # -------===[ History ]===------- #
+        setopt HIST_BEEP                    # Beep on non-existent history access.
+        setopt HIST_EXPIRE_DUPS_FIRST       # Expire duplicate entries first.
+        setopt HIST_FIND_NO_DUPS            # Don't display previously found entries.
+        setopt HIST_IGNORE_ALL_DUPS         # No duplicate entries!
+        setopt HIST_IGNORE_DUPS             # Don't record entries twice in a row.
+        setopt HIST_IGNORE_SPACE            # Don't record whitespace entries.
+        setopt HIST_REDUCE_BLANKS           # Remove superfluous blanks before recording entry.
+        setopt HIST_SAVE_NO_DUPS            # Don't write duplicate entires.
+        setopt HIST_VERIFY                  # Don't execute on expansion!
 
         # -------===[ Jobs ]===------- #
         setopt LONG_LIST_JOBS               # Long format job list.
@@ -85,70 +113,45 @@ in {
         unsetopt GLOB_DOTS
         unsetopt AUTO_NAME_DIRS             # Don't add variable-stored paths to ~ list
 
+        # -------===[ Colored Man-Pages ]===------- #
+        export LESS_TERMCAP_md=$'\e[01;31m'
+        export LESS_TERMCAP_me=$'\e[0m'
+        export LESS_TERMCAP_se=$'\e[0m'
+        export LESS_TERMCAP_so=$'\e[01;44;33m'
+        export LESS_TERMCAP_ue=$'\e[0m'
+        export LESS_TERMCAP_us=$'\e[1;33m't
+
         # -------===[ External Plugins ]===------- #
         eval "$(zoxide init zsh)"
-
-        # Solution <- Invalid subscript range
-        # typeset -A ZSH_HIGHLIGHT_STYLES
-
-        # -------===[ Theming ]===------- #
-        if [[ -f "$HOME/.config/zsh/${themeCfg.active}.zsh" ]]; then
-          source "$HOME/.config/zsh/${themeCfg.active}.zsh"
-        fi
-
-        if [[ -f "$HOME/.config/zsh/fzf.zsh" ]]; then
-          source "$HOME/.config/zsh/fzf.zsh"
-        fi
       '';
 
       shellAliases = {
         exa = "exa --group-directories-first";
       };
 
-      plugins = with pkgs; [
-        {
-          name = "zsh-nix-shell";
-          src = zsh-nix-shell.src;
-        }
-        {
-          name = "zsh-vi-mode";
-          src = zsh-vi-mode.src;
-        }
-        {
-          name = "zsh-fzf-tab"; # TODO: Source before auto-suggestion
-          src = zsh-fzf-tab.src;
-        }
-        {
-          name = "zsh-syntax-highlighting";
-          src = zsh-syntax-highlighting.src;
-        }
-        # {
-        #   name = "zsh-autocomplete";
-        #   src = zsh-autocomplete.src;
-        # }
-        {
-          name = "zsh-autosuggestions";
-          src = zsh-autosuggestions.src;
-        }
-
-        {
-          name = "zsh-autopair";
-          src = zsh-autopair.src;
-        }
-        {
-          name = "zsh-you-should-use";
-          src = zsh-you-should-use.src;
-        }
-        {
-          name = "zsh-abbr";
-          src = pkgs.fetchFromGitHub {
-            owner = "olets";
-            repo = "zsh-abbr";
-            rev = "v4.8.0";
-            sha256 = "diitszKbu530zXbJx4xmfOjLsITE9ucmWdsz9VTXsKg=";
-          };
-        }
-      ];
+      plugins = let
+        mkPlugin = name: {
+          inherit name;
+          inherit (pkgs."zsh-${name}") src;
+        };
+      in
+        [
+          {
+            name = "zsh-abbr";
+            src = pkgs.fetchFromGitHub {
+              owner = "olets";
+              repo = "zsh-abbr";
+              rev = "v4.8.0";
+              sha256 = "diitszKbu530zXbJx4xmfOjLsITE9ucmWdsz9VTXsKg=";
+            };
+          }
+        ]
+        ++ (builtins.map (p: mkPlugin p) [
+          "autopair"
+          "nix-shell"
+          "vi-mode"
+          "you-should-use"
+        ]);
     };
 
     home.configFile = mkMerge [
@@ -162,15 +165,13 @@ in {
       (mkIf (themeCfg.active != null) {
         "zsh/fzf.zsh".text = with themeCfg.colors.main; ''
           export FZF_DEFAULT_OPTS=" \
-          --color=bg+:${normal.black},bg:${types.bg},spinner:${types.highlight},hl:${normal.red} \
-          --color=fg:${types.border},header:${normal.red},info:${normal.magenta},pointer:${types.highlight} \
-          --color=marker:${types.highlight},fg+:${types.border},prompt:${normal.magenta},hl+:${normal.red}"
+          --color=bg:,bg+:${types.bg},spinner:${types.panelbg},hl:${normal.red} \
+          --color=fg:${types.border},header:${normal.red},info:${normal.magenta},pointer:${types.border} \
+          --color=marker:${normal.magenta},fg+:${types.border},prompt:${types.border},hl+:${normal.red}"
         '';
 
+        #  Reference: https://github.com/zsh-users/zsh-syntax-highlighting/blob/master/docs/highlighters/main.md
         "zsh/${themeCfg.active}.zsh".text = with themeCfg.colors.main; ''
-          # -------===[ Reference ]===------- #
-          #  Reference: https://github.com/zsh-users/zsh-syntax-highlighting/blob/master/docs/highlighters/main.md
-
           # -------===[ Comments ]===------- #
           ZSH_HIGHLIGHT_STYLES[comment]='fg=${normal.black}'
 
@@ -217,7 +218,6 @@ in {
           ZSH_HIGHLIGHT_STYLES[named-fd]='fg=${bright.cyan}'
           ZSH_HIGHLIGHT_STYLES[numeric-fd]='fg=${bright.cyan}'
 
-
           # -------===[ Non-Exclusive ]===------- #
           ZSH_HIGHLIGHT_STYLES[unknown-token]='fg=${normal.red}'
           ZSH_HIGHLIGHT_STYLES[path]='fg=${bright.cyan},underline'
@@ -233,7 +233,7 @@ in {
           ZSH_HIGHLIGHT_STYLES[cursor]='fg=${bright.cyan}'
 
           # -------===[ Plugins ]===------- #
-          ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=${bright.black},bg=${types.bg},bold,underline"
+          ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=${bright.black},bold,underline"
         '';
       })
     ];
