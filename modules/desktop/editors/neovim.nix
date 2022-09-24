1372d4aa048d62da8c6e0c49194ac94e06dc3a46
Icy-Thought <icy-thought@pm.me>
2022-09-24 14:03:11 +0200

    [𝝺] (neovim): updating submodule

--- modules/desktop/editors/neovim.nix
+++ modules/desktop/editors/neovim.nix
@@ -69,7 +69,7 @@ in {
       #       vim.g.sqlite_clib_path = "${pkgs.sqlite.out}/lib/libsqlite3.so"
       #
       #       -- Call-forward Agasaya:
-      #       require("core")
+      #       require("core").init()
       #     '';
       #   };
       # };
