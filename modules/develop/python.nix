d9d2f82ea01366afc2e7b7b32a878dd89a23d972
Icy-Thought <icy-thought@pm.me>
2022-11-08 15:28:15 +0100

    [𝚿] (python): back to testing mypy again.. lol

--- modules/develop/python.nix
+++ modules/develop/python.nix
@@ -12,20 +12,20 @@ with lib.my; {
 
   config = mkMerge [
     (mkIf config.modules.develop.python.enable {
-      user.packages = with pkgs; [
-        python3
-        nodePackages.pyright
-      ] ++ (with python3Packages; [
-        # Code-Formatter
-        black
-        isort
-
-        # Toolset
-        ipython
-        pip
-        poetry
-        setuptools
-      ]);
+      user.packages = with pkgs;
+        [ python3 ]
+        ++ (with python3Packages; [
+          mypy
+          flake8
+          python-lsp-server
+          pylsp-mypy
+          black
+          isort
+          ipython
+          pip
+          poetry
+          types-setuptools
+        ]);
 
       environment.shellAliases = {
         py = "python";
