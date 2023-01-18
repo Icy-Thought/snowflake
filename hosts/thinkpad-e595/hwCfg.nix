3568845a318aca47655318178442336a73a6221c
Icy-Thought <icy-thought@pm.me>
2023-01-18 21:00:01 +0100

    [𝚵] (thinkpad): testing out new hwCfg

--- hosts/thinkpad-e595/hwCfg.nix
+++ hosts/thinkpad-e595/hwCfg.nix
@@ -61,8 +61,21 @@ in {
     # updateMicrocode = mkDefault config.hardware.enableRedistributableFirmware;
   };
 
-  powerManagement = {
-    cpuFreqGovernor = "schedutil";
+  # Improving laptop battery-life:
+  services = {
+    power-profiles-daemon.enable = false;
+    thermald.enable = true;
+    tlp = {
+      enable = true;
+      settings = {
+        START_CHARGE_THRESH_BAT0 = 75;
+        STOP_CHARGE_THRESH_BAT0 = 80;
+        CPU_BOOST_ON_AC = 1;
+        CPU_BOOST_ON_BAT = 0;
+        CPU_SCALING_GOVERNOR_ON_AC = "performance";
+        CPU_SCALING_GOVERNOR_ON_BAT = "schedutil";
+      };
+    };
   };
 
   # Finally, our beloved hardware module(s):
