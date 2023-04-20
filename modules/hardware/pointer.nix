553654bae45f13d9e60b57626b3f0ae7ee6a1472
Icy-Thought <icy-thought@pm.me>
2023-04-20 11:15:04 +0200

    [𝚫] (pointer): `imwheel` -> borked experience..

--- modules/hardware/pointer.nix
+++ modules/hardware/pointer.nix
@@ -6,40 +6,39 @@ let
 in {
   options.modules.hardware.pointer = { enable = mkBoolOpt false; };
 
-  config = mkIf config.modules.hardware.pointer.enable (mkMerge [
-    {
-      services.xserver.libinput = {
-        enable = true;
-        mouse = {
-          middleEmulation = false;
-          disableWhileTyping = true;
-        };
-        touchpad = {
-          sendEventsMode = "disabled-on-external-mouse";
-          scrollMethod = "twofinger";
-          naturalScrolling = true;
-          tapping = true;
-          tappingDragLock = false;
-          disableWhileTyping = true;
-        };
+  config = mkIf config.modules.hardware.pointer.enable (mkMerge [{
+    services.xserver.libinput = {
+      enable = true;
+      mouse = {
+        middleEmulation = false;
+        disableWhileTyping = true;
       };
-    }
+      touchpad = {
+        sendEventsMode = "disabled-on-external-mouse";
+        scrollMethod = "twofinger";
+        naturalScrolling = true;
+        tapping = true;
+        tappingDragLock = false;
+        disableWhileTyping = true;
+      };
+    };
+  }
 
-    (mkIf (config.modules.desktop.envProto == "x11") {
-      services.xserver.imwheel = {
-        enable = true;
-        extraOptions = [ "--buttons=45" ];
-        rules = {
-          "epiphany|chromium|discord" = ''
-            None,      Up,   Button4, 4
-            None,      Down, Button5, 4
-            Shift_L,   Up,   Shift_L|Button4, 4
-            Shift_L,   Down, Shift_L|Button5, 4
-            Control_L, Up,   Control_L|Button4
-            Control_L, Down, Control_L|Button5
-          '';
-        };
-      };
-    })
-  ]);
+  # (mkIf (config.modules.desktop.envProto == "x11") {
+  #   services.xserver.imwheel = {
+  #     enable = true;
+  #     extraOptions = [ "--buttons=45" ];
+  #     rules = {
+  #       "epiphany|chromium|discord" = ''
+  #         None,      Up,   Button4, 4
+  #         None,      Down, Button5, 4
+  #         Shift_L,   Up,   Shift_L|Button4, 4
+  #         Shift_L,   Down, Shift_L|Button5, 4
+  #         Control_L, Up,   Control_L|Button4
+  #         Control_L, Down, Control_L|Button5
+  #       '';
+  #     };
+  #   };
+  # })
+    ]);
 }
