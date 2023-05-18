0bd921ac474832b1c132be68e13442e641e1b78b
Icy-Thought <icy-thought@pm.me>
2023-05-19 00:40:16 +0200

    [𝚫] (wezterm): statusbar redesign

--- config/wezterm/statusbar/rose-pine.lua
+++ config/wezterm/statusbar/rose-pine.lua
@@ -13,27 +13,31 @@ wez.on("update-right-status", function(window, pane)
     window:set_left_status(wez.format({
         { Background = { Color = "#9ccfd8" } },
         { Foreground = { Color = "#191724" } },
-        { Text = "     " },
+        { Text = "    " },
+    }) .. wez.format({
+        { Background = { Color = "#191724" } },
+        { Foreground = { Color = "#9ccfd8" } },
+        { Text = "" },
     }))
 end)
 
 wez.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
     local background = "#191724"
     local foreground = "#e0def4"
-    local symbolic = " ○ "
+    local symbolic = " "
 
     if tab.is_active then
         background = "#1a1b26"
         foreground = "#bb7b79"
-        symbolic = " 綠 "
+        symbolic = " 󰪥"
     elseif hover then
         background = "#3b3052"
         foreground = "#9ccfd8"
     end
 
     local edge_background = background
-    local edge_foreground = "#c5b1e5"
-    local separator = " | "
+    local edge_foreground = "#ebbcba"
+    local separator = " ⊣ "
 
     -- ensure that the titles fit in the available space,
     -- and that we have room for the edges.
