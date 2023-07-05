20ad27de264582541a9a20f20216a15ccf9947ef
Icy-Thought <icy-thought@pm.me>
2023-07-05 22:45:48 +0200

    [𝚫] (penrose): added xephyr.sh for testing

--- packages/orcinusWM/flake.nix
+++ packages/orcinusWM/flake.nix
@@ -3,6 +3,7 @@
     cargo2nix.url = "github:cargo2nix/cargo2nix/release-0.11.0";
     flake-utils.follows = "cargo2nix/flake-utils";
     nixpkgs.follows = "cargo2nix/nixpkgs";
+    rust.url = "github:oxalica/rust-overlay";
   };
 
   outputs = inputs:
@@ -24,16 +25,41 @@
                   name = "orcinus-wm";
                   overrideAttrs = drv: {
                     propagatedNativeBuildInputs =
-                      drv.propagatedNativeBuildInputs or [] ++ [pkgs.xorg.libxcb];
+                      drv.propagatedNativeBuildInputs
+                      or []
+                      ++ [
+                        pkgs.xorg.libxcb
+                        pkgs.xorg.xmodmap
+                      ];
                   };
                 })
               ];
           };
+
+          workspaceShell = rustPkgs.workspaceShell {
+            buildInputs = with pkgs; [
+              rust-analyzer
+              xorg.libxcb
+              xorg.xmodmap
+              alejandra
+            ];
+
+            nativeBuildInputs = with pkgs; [
+              rust-bin.stable.latest.default
+            ];
+          };
         in rec {
+          # Executed by `nix build`
           packages = {
             orcinusWM = (rustPkgs.workspace.orcinus-wm {}).bin;
             default = packages.orcinusWM;
           };
+
+          # Used by `nix develop`
+          devShells.default = workspaceShell;
+
+          # Executed by `nix run` :TODO| xephyr script on run
+          # apps.default = packages.orcinusWM;
         }
       );
 }
