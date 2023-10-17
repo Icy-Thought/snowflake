015824693fbc34708d93984eff98766e6210a7be
Icy-Thought <icy-thought@pm.me>
2023-10-17 15:38:12 +0200

    [𝚫] (develop): populated pythonrc, ipy == gone.. bye!

--- modules/develop/python.nix
+++ modules/develop/python.nix
@@ -7,44 +7,104 @@
 }: let
   inherit (lib.attrsets) attrValues;
   inherit (lib.modules) mkIf mkMerge;
+  cfg = config.modules.develop.python;
 in {
   options.modules.develop.python = let
     inherit (lib.options) mkEnableOption;
-  in {enable = mkEnableOption "Python development";};
+  in {
+    enable = mkEnableOption "Python development";
+    autoREPL.enable = mkEnableOption "Python REPL framework";
+    initREPL.enable = mkEnableOption "Manual configuration of Python REPL" // {default = cfg.enable;};
+  };
 
   config = mkMerge [
-    (mkIf config.modules.develop.python.enable {
+    (mkIf cfg.enable {
       user.packages = attrValues {
-        inherit (pkgs) python3 rye ruff;
+        inherit (pkgs) rye ruff;
         inherit
           (pkgs.python3Packages)
           black
           isort
-          ipython
           mypy
           python-lsp-server
           ;
-      };
-
-      environment.shellAliases = {
-        py = "python";
-        pip = "rye";
-        ipy = "ipython --no-banner";
-        ipylab = "ipython --pylab=qt5 --no-banner";
       };
 
       hm.programs.vscode.extensions = attrValues {
         inherit (pkgs.vscode-extensions.ms-python) python;
         inherit (pkgs.vscode-extensions.ms-toolsai) jupyter;
       };
-    })
 
-    (mkIf config.modules.develop.xdg.enable {
-      env = {
-        IPYTHONDIR = "$XDG_CONFIG_HOME/ipython";
-        PYTHONSTARTUP = "$XDG_CONFIG_HOME/python/pythonrc";
-        JUPYTER_CONFIG_DIR = "$XDG_CONFIG_HOME/jupyter";
+      environment.shellAliases = {
+        py = "python";
+        pip = "rye";
       };
     })
+
+    (mkIf config.modules.develop.xdg.enable (mkMerge [
+      {
+        env = {
+          PYTHONSTARTUP = "$XDG_CONFIG_HOME/python/pythonrc";
+          PYTHON_HISTORY_FILE = "$XDG_CONFIG_HOME/python/history";
+          JUPYTER_CONFIG_DIR = "$XDG_CONFIG_HOME/jupyter";
+        };
+      }
+
+      (mkIf cfg.autoREPL.enable {
+        user.packages = attrValues {
+          inherit (pkgs) python3;
+          inherit (pkgs.python3Packages) bpython;
+        };
+
+        environment.shellAliases = {
+          bpy = "bpython";
+        };
+
+        home.configFile.pythonRC = {
+          target = "python/pythonrc";
+          text = "";
+        };
+      })
+
+      (mkIf cfg.initREPL.enable {
+        user.packages = [
+          (pkgs.python3.withPackages (pyPkgs: with pyPkgs; [rich]))
+        ];
+
+        home.configFile.pythonRC = {
+          target = "python/pythonrc";
+          text = ''
+            def configure_rich_repl():
+                try:
+                    from rich import print, pretty, traceback
+                    from rich.logging import RichHandler
+                except ImportError:
+                   print("Module 'rich' not available.")
+
+                pretty.install(); traceback.install()
+
+                from logging import getLogger
+                getLogger().addHandler(RichHandler())
+
+                print("Rich REPL integration == successful!")
+
+
+            def configure_comp_repl():
+                try:
+                    import readline
+                except ImportError:
+                   print("Module readline not available.")
+                else:
+                   import rlcompleter
+                   readline.parse_and_bind("tab: complete")
+
+
+            if __name__ == "__main__":
+                configure_comp_repl()
+                configure_rich_repl()
+          '';
+        };
+      })
+    ]))
   ];
 }
