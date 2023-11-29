3074f44997b49cc3168743ab6f7aecc03ed0bd28
Icy-Thought <icy-thought@pm.me>
2023-11-29 01:58:09 +0100

    [𝚫] (python): unifying prev. seperated settings -> enabled

--- modules/develop/python.nix
+++ modules/develop/python.nix
@@ -11,18 +11,15 @@
 in {
   options.modules.develop.python = let
     inherit (lib.options) mkEnableOption;
-  in {
-    enable = mkEnableOption "Python development";
-    autoREPL.enable = mkEnableOption "Python REPL framework" // {default = cfg.enable;};
-    initREPL.enable = mkEnableOption "Manual configuration of Python REPL";
-  };
+  in {enable = mkEnableOption "Python development";};
 
   config = mkIf cfg.enable (mkMerge [
     {
       user.packages = attrValues {
+        rich-env = pkgs.python3.withPackages (pyPkgs: with pyPkgs; [rich]);
         inherit (pkgs) rye;
         inherit (pkgs.nodePackages) pyright;
-        inherit (pkgs.python3Packages) black isort;
+        inherit (pkgs.python3Packages) ipython black isort;
       };
 
       hm.programs.vscode.extensions = attrValues {
@@ -33,80 +30,52 @@ in {
       environment.shellAliases = {
         py = "python";
         pip = "rye";
+        ipy = "ipython --no-banner";
+        ipylab = "ipython --pylab=qt5 --no-banner";
       };
     }
 
-    (mkIf config.modules.develop.xdg.enable (mkMerge [
-      {
-        env = {
-          PYTHONSTARTUP = "$XDG_CONFIG_HOME/python/pythonrc";
-          PYTHON_HISTORY_FILE = "$XDG_CONFIG_HOME/python/history";
-          JUPYTER_CONFIG_DIR = "$XDG_CONFIG_HOME/jupyter";
-        };
-      }
+    (mkIf config.modules.develop.xdg.enable {
+      env = {
+        PYTHONSTARTUP = "$XDG_CONFIG_HOME/python/pythonrc";
+        PYTHON_HISTORY_FILE = "$XDG_CONFIG_HOME/python/history";
+        JUPYTER_CONFIG_DIR = "$XDG_CONFIG_HOME/jupyter";
+        IPYTHONDIR = "$XDG_CONFIG_HOME/ipython";
+      };
 
-      (mkIf cfg.autoREPL.enable {
-        user.packages = attrValues {
-          inherit (pkgs) python3;
-          inherit (pkgs.python3Packages) ipython;
-        };
+      home.configFile.pythonRC = {
+        target = "python/pythonrc";
+        text = ''
+          def configure_rich_repl():
+              try:
+                  from rich import print, pretty, traceback
+                  from rich.logging import RichHandler
+              except ImportError:
+                 print("Module 'rich' not available.")
 
-        home.configFile.pythonRC = {
-          target = "python/pythonrc";
-          text = "";
-        };
+              pretty.install(); traceback.install()
 
-        env = {
-          IPYTHONDIR = "$XDG_CONFIG_HOME/ipython";
-          PYTHONSTARTUP = "$XDG_CONFIG_HOME/python/pythonrc";
-          JUPYTER_CONFIG_DIR = "$XDG_CONFIG_HOME/jupyter";
-        };
+              from logging import getLogger
+              getLogger().addHandler(RichHandler())
 
-        environment.shellAliases = {
-          ipy = "ipython --no-banner";
-          ipylab = "ipython --pylab=qt5 --no-banner";
-        };
-      })
-
-      (mkIf cfg.initREPL.enable {
-        user.packages = [
-          (pkgs.python3.withPackages (pyPkgs: with pyPkgs; [rich]))
-        ];
-
-        home.configFile.pythonRC = {
-          target = "python/pythonrc";
-          text = ''
-            def configure_rich_repl():
-                try:
-                    from rich import print, pretty, traceback
-                    from rich.logging import RichHandler
-                except ImportError:
-                   print("Module 'rich' not available.")
-
-                pretty.install(); traceback.install()
-
-                from logging import getLogger
-                getLogger().addHandler(RichHandler())
-
-                print("Rich REPL integration == successful!")
+              print("Rich REPL integration == successful!")
 
 
-            def configure_comp_repl():
-                try:
-                    import readline
-                except ImportError:
-                   print("Module readline not available.")
-                else:
-                   import rlcompleter
-                   readline.parse_and_bind("tab: complete")
+          def configure_comp_repl():
+              try:
+                  import readline
+              except ImportError:
+                 print("Module readline not available.")
+              else:
+                 import rlcompleter
+                 readline.parse_and_bind("tab: complete")
 
 
-            if __name__ == "__main__":
-                configure_comp_repl()
-                configure_rich_repl()
-          '';
-        };
-      })
-    ]))
+          if __name__ == "__main__":
+              configure_comp_repl()
+              configure_rich_repl()
+        '';
+      };
+    })
   ]);
 }
