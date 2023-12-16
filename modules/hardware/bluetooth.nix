4890d4ad0f1cb42df6db6f5c746d59499e1558fe
Icy-Thought <icy-thought@pm.me>
2023-12-16 03:14:52 +0100

    [𝚫] (bluetooth): media control through `mpris-proxy`

--- modules/hardware/bluetooth.nix
+++ modules/hardware/bluetooth.nix
@@ -19,5 +19,12 @@ in {
       enable = true;
       disabledPlugins = ["sap"];
     };
+
+    systemd.user.services.mpris-proxy = {
+      description = "mpris-proxy -> bluetooth (media) ctrl";
+      after = ["network.target" "sound.target"];
+      wantedBy = ["default.target"];
+      serviceConfig.ExecStart = "${pkgs.bluez}/bin/mpris-proxy";
+    };
   };
 }
