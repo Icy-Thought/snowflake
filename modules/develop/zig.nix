1c33f685278719942b82ed88818fdab68c19566c
Icy-Thought <icy-thought@pm.me>
2024-02-23 04:03:25 +0100

    [𝚫] (develop): testing out zig-lang

---
+++ modules/develop/zig.nix
@@ -0,0 +1,33 @@
+{
+  inputs,
+  config,
+  options,
+  lib,
+  pkgs,
+  ...
+}: let
+  inherit (lib.attrsets) attrValues;
+  inherit (lib.modules) mkIf mkMerge;
+in {
+  options.modules.develop.zig = let
+    inherit (lib.options) mkEnableOption;
+  in {enable = mkEnableOption "Zig development";};
+
+  config = mkMerge [
+    (mkIf config.modules.develop.zig.enable {
+      user.packages = attrValues {
+        inherit (pkgs) zig zls;
+      };
+
+      hm.programs.vscode.extensions = attrValues {
+        inherit (pkgs.vscode-extensions.ziglang) vscode-zig;
+      };
+    })
+
+    (mkIf config.modules.develop.xdg.enable {
+      # env = {
+      #   ZIG_GLOBAL_CACHE_DIR = "$TMPDIR/zig-cache";
+      # };
+    })
+  ];
+}
