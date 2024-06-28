d19007c5cd76afd10dafbaf9b3fd4c852bb21681
Icy-Thought <icy-thought@pm.me>
2024-06-28 20:16:59 +0200

    [Packages] fixed tokyonight

--- packages/tokyonight-gtk/default.nix
+++ packages/tokyonight-gtk/default.nix
@@ -4,48 +4,65 @@
   fetchFromGitHub,
   gtk-engine-murrine,
   jdupes,
-  variant ? "Dark-BL",
+  sassc,
+  accent ? ["default"],
+  shade ? "dark",
+  size ? "standard",
 }: let
-  inherit (builtins) toString;
-  inherit (lib.trivial) checkListOfEnum;
-  validVariants = ["Dark-B-LB" "Dark-B" "Dark-BL-LB" "Dark-BL" "Storm-B-LB" "Storm-B" "Storm-BL-LB" "Storm-BL"];
+  validAccents = ["default" "purple" "pink" "red" "orange" "yellow" "green" "teal" "grey"];
+  validShades = ["light" "dark"];
+  validSizes = ["standard" "compact"];
 
-  pname = "tokyonight-gtk-theme";
+  single = x: lib.optional (x != null) x;
+  pname = "Tokyonight";
 in
-  checkListOfEnum "${pname}: GTK Theme Variants" validVariants [variant]
+  lib.checkListOfEnum "${pname} Valid theme accent(s)" validAccents accent
+  lib.checkListOfEnum "${pname} Valid shades" validShades (single shade)
+  lib.checkListOfEnum "${pname} Valid sizes" validSizes (single size)
+
   stdenv.mkDerivation {
-    pname = "tokyonight-gtk-theme";
-    version = "unstable-2022-12-09";
+    pname = "${pname}";
+    version = "unstable-2024-06-28";
 
     src = fetchFromGitHub {
       owner = "Fausto-Korpsvart";
-      repo = "Tokyo-Night-GTK-Theme";
-      rev = "d17eec24180b890bc4a9aa64162074b1bfc7258a";
-      hash = "sha256-b35J6NsFkUNM/yxMe7bi0kpyuI/pGLnCywCEDLHLf5A=";
+      repo = "Tokyonight-GTK-Theme";
+      rev = "2f566d89856516bef988df3cc32261f752299886";
+      hash = "sha256-oKqLb66N4swHfhjUZJIGryE0D9MkuLdKFQa6j3TFmOg=";
     };
 
-    nativeBuildInputs = [jdupes];
+    nativeBuildInputs = [jdupes sassc];
 
     propagatedUserEnvPkgs = [gtk-engine-murrine];
 
-    installPhase = let
-      gtkTheme = "Tokyonight-${toString variant}";
-    in ''
+    postPatch = ''
+      find -name "*.sh" -print0 | while IFS= read -r -d ''' file; do
+        patchShebangs "$file"
+      done
+    '';
+
+    dontBuild = true;
+
+    installPhase = ''
       runHook preInstall
 
       mkdir -p $out/share/themes
 
-      cp -r $src/themes/${gtkTheme} $out/share/themes
+      ./themes/install.sh \
+        --name ${pname} \
+        ${toString (map (x: "--theme " + x) accent)} \
+        ${lib.optionalString (shade != null) ("--color " + shade)} \
+        ${lib.optionalString (size != null) ("--size " + size)} \
+        --dest $out/share/themes
 
-      # Duplicate files -> hard-links = reduced install-size!
-      jdupes -L -r $out/share
+      jdupes --quiet --link-soft --recurse $out/share
 
       runHook postInstall
     '';
 
     meta = with lib; {
       description = "A GTK theme based on the Tokyo Night colour palette";
-      homepage = "https://github.com/Fausto-Korpsvart/Tokyo-Night-GTK-Theme";
+      homepage = "https://github.com/Fausto-Korpsvart/Tokyonight-GTK-Theme";
       license = licenses.gpl3Only;
       # maintainers = [ Icy-Thought ];
       platforms = platforms.all;
