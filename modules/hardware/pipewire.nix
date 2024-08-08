b8112ef84f53c5821ad58b096cf4b3380b591515
Icy-Thought <icy-thought@pm.me>
2024-08-08 02:14:32 +0200

    [Pipewire] Updating to new config format

--- modules/hardware/pipewire.nix
+++ modules/hardware/pipewire.nix
@@ -23,42 +23,25 @@ in {
 
     services.pipewire = {
       enable = true;
-      wireplumber.enable = true;
       alsa = {
         enable = true;
         support32Bit = true;
       };
       pulse.enable = true;
-    };
+      wireplumber = {
+        enable = true;
+        extraConfig = {
+          "monitor.bluez.properties" = {
+            "bluez5.enable-sbc-xq" = true;
+            "bluez5.enable-msbc" = true;
+            "bluez5.enable-hw-volume" = true;
+            "bluez5.roles" = ["hsp_hs" "hsp_ag" "hfp_hf" "hfp_ag"];
+          };
 
-    create.configFile = mkIf config.modules.hardware.bluetooth.enable {
-      wireplumber-bluetooth = {
-        target = "wireplumber/bluetooth.lua.d/51-bluez-config.lua";
-        text = ''
-          bluez_monitor.properties = {
-              ["bluez5.enable-sbc-xq"] = true,
-              ["bluez5.enable-msbc"] = true,
-              ["bluez5.enable-hw-volume"] = true,
-              ["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
-          }
-        '';
-      };
-
-      wireplumber-disable-suspension = {
-        target = "wireplumber/main.lua.d/51-disable-suspension.lua";
-        text = ''
-          table.insert(alsa_monitor.rules, {
-              matches = {
-                  { -- Matches all sources.
-                      { "node.name", "matches", "alsa_input.*" },
-                  },
-                  { -- Matches all sinks.
-                      { "node.name", "matches", "alsa_output.*" },
-                  },
-              },
-              apply_properties = { ["session.suspend-timeout-seconds"] = 0 },
-          })
-        '';
+          "wireplumber.profiles" = {
+            main."monitor.libcamera" = "disabled";
+          };
+        };
       };
     };
   };
