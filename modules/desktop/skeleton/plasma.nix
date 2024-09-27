e69434f123eb4348a2840b7c90aa7e47b5d227eb
Icy-Thought <icy-thought@pm.me>
2024-09-27 23:57:12 +0200

    [Skeleton] oops.. `mkMerge` it is (laziness prohibited)

--- modules/desktop/skeleton/plasma.nix
+++ modules/desktop/skeleton/plasma.nix
@@ -2,34 +2,38 @@
 
 let
   inherit (lib.attrsets) attrValues;
-  inherit (lib.modules) mkIf;
+  inherit (lib.modules) mkIf mkMerge;
 in {
   options.modules.desktop.plasma = let inherit (lib.options) mkEnableOption;
   in { enable = mkEnableOption "modern desktop environment"; };
 
-  config = mkIf config.modules.desktop.plasma.enable {
-    modules.desktop = {
-      type = "wayland";
-      extensions.input-method = {
-        enable = true;
-        framework = "fcitx5";
+  config = mkMerge [
+    (mkIf config.modules.desktop.plasma.enable {
+      modules.desktop = {
+        type = "wayland";
+        extensions.input-method = {
+          enable = true;
+          framework = "fcitx5";
+        };
       };
-    };
 
-    services.desktopManager.plasma6 = {
-      enable = true;
-      enableQt5Integration = false;
-    };
-    services.greetd.settings.initial_session.command = "plasmawayland";
+      services.desktopManager.plasma6 = {
+        enable = true;
+        enableQt5Integration = false;
+      };
+      services.greetd.settings.initial_session.command = "plasmawayland";
 
-    environment.plasma6.excludePackages =
-      attrValues { inherit (pkgs.kdePackages) konsole oxygen; };
-  };
-  programs.dconf.enable = true;
+      environment.plasma6.excludePackages =
+        attrValues { inherit (pkgs.kdePackages) konsole oxygen; };
+      programs.dconf.enable = true;
+    })
 
-  environment.sessionVariables = mkIf (config.desktop.type == "wayland") {
-    ELECTRON_OZONE_PLATFORM_HINT = "auto";
-    NIXOS_OZONE_WL = "1";
-    MOZ_ENABLE_WAYLAND = "1";
-  };
+    (mkIf (config.modules.desktop.type == "wayland") {
+      environment.sessionVariables = {
+        ELECTRON_OZONE_PLATFORM_HINT = "auto";
+        NIXOS_OZONE_WL = "1";
+        MOZ_ENABLE_WAYLAND = "1";
+      };
+    })
+  ];
 }
