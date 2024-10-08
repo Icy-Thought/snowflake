3ff28b710e383473043406a1212fdd8a96767c20
Icy-Thought <icy-thought@pm.me>
2024-10-08 14:28:31 +0200

    [FMT] tabs -> spaces (people hate tabs :/)

--- modules/develop/lua.nix
+++ modules/develop/lua.nix
@@ -7,19 +7,30 @@ let
 in {
   options.modules.develop.lua = let inherit (lib.options) mkEnableOption;
   in {
-	enable = mkEnableOption "Lua development";
-	fennel.enable = mkEnableOption "Lisp-based Lua development";
+    enable = mkEnableOption "Lua development";
+    fennel.enable = mkEnableOption "Lisp-based Lua development";
   };
 
   config = mkIf cfg.enable (mkMerge [
-	{
-	  user.packages = attrValues ({
-		inherit (pkgs) lua lua-language-server stylua;
-	  } // optionalAttrs (cfg.fennel.enable) { inherit (pkgs) fennel fnlfmt; });
-	}
+    {
+      user.packages = attrValues ({
+        inherit (pkgs) lua lua-language-server stylua;
+      } // optionalAttrs (cfg.fennel.enable) { inherit (pkgs) fennel fnlfmt; });
+    }
 
-	(mkIf config.modules.develop.xdg.enable {
-	  # TODO
-	})
+    (mkIf config.modules.develop.xdg.enable {
+      create.configFile.stylua-conf = {
+        target = "stylua/stylua.toml";
+        source = let tomlFormat = pkgs.formats.toml { };
+        in tomlFormat.generate "stylua-conf" {
+          column_width = 80;
+          line_endings = "Unix";
+          indent_type = "Spaces";
+          indent_width = 4;
+          quote_style = "AutoPreferDouble";
+          call_parentheses = "Always";
+        };
+      };
+    })
   ]);
 }
