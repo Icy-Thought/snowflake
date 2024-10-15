41c91fefa03c6933f6b1d3147884449fc9a6959b
Icy-Thought <icy-thought@pm.me>
2024-10-15 21:29:51 +0200

    [HW] hide pointer when inactive

--- modules/hardware/pointer.nix
+++ modules/hardware/pointer.nix
@@ -5,39 +5,32 @@ in {
   options.modules.hardware.pointer = let inherit (lib.options) mkEnableOption;
   in { enable = mkEnableOption "pointer control"; };
 
-  config = mkIf config.modules.hardware.pointer.enable (mkMerge [{
-    services.libinput = {
-      enable = true;
-      mouse = {
-        middleEmulation = false;
-        disableWhileTyping = true;
+  config = mkIf config.modules.hardware.pointer.enable (mkMerge [
+    {
+      services.libinput = {
+        enable = true;
+        mouse = {
+          middleEmulation = false;
+          disableWhileTyping = true;
+        };
+        touchpad = {
+          sendEventsMode = "disabled-on-external-mouse";
+          scrollMethod = "twofinger";
+          naturalScrolling = true;
+          tapping = true;
+          tappingDragLock = false;
+          disableWhileTyping = true;
+        };
       };
-      touchpad = {
-        sendEventsMode = "disabled-on-external-mouse";
-        scrollMethod = "twofinger";
-        naturalScrolling = true;
-        tapping = true;
-        tappingDragLock = false;
-        disableWhileTyping = true;
-      };
-    };
-  }
+    }
 
-  # (mkIf (config.modules.desktop.type == "x11") {
-  #   services.xserver.imwheel = {
-  #     enable = true;
-  #     extraOptions = [ "--buttons=45" ];
-  #     rules = {
-  #       "epiphany|chromium|discord" = ''
-  #         None,      Up,   Button4, 4
-  #         None,      Down, Button5, 4
-  #         Shift_L,   Up,   Shift_L|Button4, 4
-  #         Shift_L,   Down, Shift_L|Button5, 4
-  #         Control_L, Up,   Control_L|Button4
-  #         Control_L, Down, Control_L|Button5
-  #       '';
-  #     };
-  #   };
-  # })
-    ]);
+    (mkIf (config.modules.desktop.type == "x11") {
+      services.unclutter-xfixes = {
+        enable = true;
+        extraOptions = [ "exclude-root" "ignore-scrolling" "fork" ];
+        threshold = 1;
+        timeout = 1;
+      };
+    })
+  ]);
 }
