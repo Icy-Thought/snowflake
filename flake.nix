b42326f66e47801c0b8418bd38f6fe2bb3ebc235
Icy-Thought <icy-thought@pm.me>
2024-12-03 23:59:45 +0100

    [FLK] zen flake input back to `0xc000022070`

--- flake.nix
+++ flake.nix
@@ -27,7 +27,7 @@
 
     # Application -> (Cached) Git
     spicetify-nix.url = "github:Gerg-L/spicetify-nix";
-    zen-browser.url = "github:ch4og/zen-browser-flake";
+    zen-browser.url = "github:0xc000022070/zen-browser-flake";
     rust.url = "github:oxalica/rust-overlay";
 
     # Submodules (temporary) # TODO
